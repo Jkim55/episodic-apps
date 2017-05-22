8444c33e2f015766acfa997341ff77f743c4f554
Ji Kim <jkim55@gmail.com>
2017-05-22 10:54:21 -0600

    add products endpoint

---
+++ episodic-events/src/main/java/com/example/episodicevents/products/DigitalProduct.java
@@ -0,0 +1,22 @@
+package com.example.episodicevents.products;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class DigitalProduct extends Product {
+    private String fileUrl;
+
+    public DigitalProduct(String sku, String name, int priceInCents, String fileUrl) {
+        super(sku, name, priceInCents);
+        this.fileUrl = fileUrl;
+    }
+
+    public String getProductCategory() {
+        return "media";
+    }
+
+}
